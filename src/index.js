0af8e9a1bce55c55fb7165945811b5af5fd2a2b0
my9074 <313231068@qq.com>
2018-10-24 11:48:52 +0800

    feat(locale): Add locale use/i18n for index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 
 import Text from '../packages/text'
+import locale from './locale/index'
+
 const components = [Text]
 
-const install = function(Vue) {
+const install = function(Vue, opts = {}) {
 	if (install.installed) return
+	locale.use(opts.locale)
+	locale.i18n(opts.i18n)
 
 	components.map(component => {
 		Vue.component(component.name, component)
@@ -16,4 +20,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 export default {
 	install,
+	locale: locale.use,
+	i18n: locale.i18n,
+	Text
 }
